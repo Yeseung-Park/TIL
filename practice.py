eae402ba4620aabe43c1974b8e01491ca16f2ed3
Yeseung-Park <yspark8538@naver.com>
2024-07-31 15:36:55 +0900

    0731 TIL

--- practice.py
+++ practice.py
@@ -1,27 +1,15 @@
-num = 456789    # Baby-gin 확인할 6자리 수
-c=[0]*12    # 개수 누적 리스트  # 가장 큰 정수는 9인데 12개의 배열을 생성하는 이유는 혹시 모르니까...
+arr = [3, 6, 7, 1, 5, 4]
 
-for i in range(6):
-    c[num%10]+=1    # num을 리스트로 지정하지 않았기 때문에 이런 식으로 일의 자리 숫자부터 확인을 해준다.
-    num //= 10
+n = len(arr)
+subset_list =[]
+# << 비트 연산자는 왼쪽으로 쉬프트 한다는 것으로 왼쪽으로 쉬프트 할 경우 2배가 된다.
+for i in range(1<<n):    # 1<<n = 2의 n제곱 (1을 왼쪽으로 n만큼 쉬프트)
+    subset=[]
+    for j in range(n):
+        if i & (1<<j):
+            subset.append(arr[j])
+    subset_list.append(subset)
 
-i=0
-tri=run=0
+print(subset_list)
 
-while i<10:
-    if c[i]>=3: # triplete 조사 후 데이터 삭제
-        c[i]-=3
-        tri+=1
-        continue
-    if c[i]>=1 and c[i+1]>=1 and c[i+2]>=1:
-        c[i]-=1
-        c[i+1]-=1
-        c[i+2]=-1
-        run+=1
-        continue
-    i+=1
-
-if run + tri == 2:
-    print("Baby Gin!")
-else:
-    print("Lose")
+            
