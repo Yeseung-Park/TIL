33f9292b00e836d634a1781a6354c46d22585bde
Yeseung-Park <yspark8538@naver.com>
2024-08-02 11:16:17 +0900

    0802 TIL

--- practice.py
+++ practice.py
@@ -1,14 +1,11 @@
-def select(list, k):
-    
-    for i in range(0, k):    # 범위가 k까지인 이유는 k번째로 작은 원소만 찾으면 되기 때문
-        min_index = i
-        for j in range(i+1, len(list)):
-            if list[min_index] > list[j]:
-                min_index = j
-        list[min_index], list[i] = list[i], list[min_index]
-    
-    return list[k-1]
+def strlen(str):
+    idx = 0
+    count = 0
+    while str[idx] != '\0':
+        idx += 1
+        count += 1
+    return count
 
-list = [10, 25, 64, 22, 11]
-print(select(list, 4))    # 25
-            
+a = ['a', 'b', 'c', '\0']
+
+print(strlen(a))
