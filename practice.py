85cbcf293a37e1e3317038dd10b3fde614ffd0e3
Yeseung-Park <--global>
2024-08-07 17:51:34 +0900

    0807 TIL

--- practice.py
+++ practice.py
@@ -1,11 +1,10 @@
-def f(i, N, v):    # v: 찾고자 하는 값
-    if i == N:    # 끝까지 찾았는데 없으면 0을 반환
-        return 0
-    elif arr[i] == v:    # 찾으면 1을 반환
-        return 1
-    else:
-        return f(i+1, N, v)    # 다음 요소에 대해서 또 함수 진행
+def fibo(n):
+    f = [0]*(n+1)
+    f[0] = 0
+    f[1] = 1
+    for i in range(2, n+1):
+        f[i] = f[i-1]+f[i-2]
+    
+    return f[n]
 
-arr = [1, 2, 3, 4]
-print(f(0, 4, 3))    # 1
-print(f(0, 4, 5))    # 0
+print(fibo(7))    # 13
